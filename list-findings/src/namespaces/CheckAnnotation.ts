321ec83a41b10ac87f034d58ad02da40d5ab86f1
m-pongratz <michael.pongratz@codecentric.de>
2021-08-31 10:01:08 +0200

    feat(CIFUZZ-528): github action to list findings using Checks Api

---
+++ list-findings/src/namespaces/CheckAnnotation.ts
@@ -0,0 +1,10 @@
+export interface CheckAnnotation {
+    path: string,
+    blob_href: string,
+    annotation_level: string,
+    title: string,
+    message: string,
+    raw_details: string,
+    start_line: number,
+    end_line: number
+}
---
+++ list-findings/src/namespaces/Inputs.ts
@@ -0,0 +1,9 @@
+export interface Args {
+    repo?: string;
+    token: string;
+    serverUrl: string;
+    projectId: string;
+    ciFuzzToken: string;
+    name: string;
+    testCollectionRun: string
+}
